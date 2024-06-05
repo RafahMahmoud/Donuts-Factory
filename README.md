fd098720725496d3ef6a77e0ce9abbf024216307
RafahMahmoud <rafah.shreem98@gmail.com>
2024-06-06 00:21:16 +0300

    solve donuts factory task

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# raf
+# Donuts-Factory
+# At this folder You will find website for Donuts Factory.
+# I used HTML, CSS and JS.
+# In JS I used Prompt, Alert, Array, While loop and For loop.
---
+++ app.js
@@ -0,0 +1,16 @@
+answers = []; 
+  var ask = prompt("What do you want to order: Donuts, Coffee, Icecream, Bakery?");
+  if (ask=="Donuts" || "Coffee" || "Icecream" || "Bakery")
+    {
+    var order = prompt("Please,write your order");
+    alert(order + " is prepared");
+    console.log(fName + " " + order);
+  }
+  
+  answers.push(ask);
+  answers.push(order);
+  alert(answers);
+  for (let i = 0; i < answers.length; i++) {
+    console.log(answers[i]);
+  }
+
